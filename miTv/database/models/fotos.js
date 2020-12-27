5a30219bb2f895de6452beff6e5ec0f4a0969759
unknown <rarbtk10@gmail.com>
2020-12-26 23:44:47 -0300

    primer push con todo

---
+++ miTv/database/models/fotos.js
@@ -0,0 +1,23 @@
+const { sequelize } = require(".");
+
+module.exports = (sequelize, dataTypes) =>{
+    let alias = "imagenes";
+
+    let cols = {
+        id: { 
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+
+        },
+        descripcion: {
+            type: dataTypes.STRING
+
+        }
+    }
+    let config = {
+        tableName: "fotos",
+        timestamps: false
+    }
+}
+let imagenes = sequelize.define(alias,cols,config);
