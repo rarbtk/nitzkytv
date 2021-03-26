c6c6c7a6c368b4d865b4182fb088b31ac567e53d
unknown <rarbtk10@gmail.com>
2021-03-26 00:21:50 -0300

    se arreglaron cosas

--- miTv/routes/fotos.js
+++ miTv/routes/fotos.js
@@ -4,5 +4,13 @@ var fotosController = require("../controller/fotosController");
 
 
 router.get("/fotos", fotosController.listado);
+
+router.get("/videos", function(req,res){
+  res.render("videos")
+})
+
+router.get("/register", (req,res)=>{
+  res.render("register")
+})
   
   module.exports = router;
