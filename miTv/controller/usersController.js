fa70fb99419971fb925b4e8e4440c59de2a71ba7
unknown <rarbtk10@gmail.com>
2021-05-03 21:18:24 -0300

    se agrega el controlador de usuarios

--- miTv/controller/usersController.js
+++ miTv/controller/usersController.js
@@ -1,2 +1,26 @@
 let fs = require("fs");
 let path = require("path");
+
+const usuarios = fs.readFileSync("data/users.json");
+let usuarios_json = JSON.parse(usuarios);
+
+const usersController ={
+    listado_usuarios : (req, res) =>{
+        res.render("users", { usuarios_json})
+    },
+
+    create: (req,res)=>{
+
+    },
+
+    Storage: (req,res) =>{
+
+    },
+};
+
+
+
+
+
+
+module.exports = usersController;
--- miTv/routes/users.js
+++ miTv/routes/users.js
@@ -1,17 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+let usersController = require("../controller/usersController");
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
 
 router.get("/register", (req,res)=>{
   res.render("register")
 });
 
-router.get("/login",(req,res) =>{
-  res.render("login")
-});
+
 
 module.exports = router;
